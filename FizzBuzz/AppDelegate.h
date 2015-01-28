4237c3f3136781a0f9ee82f65c187fbe170aebbd
Julien Guanzon <julienguanzon@gmail.com>
2015-01-28 14:37:35 -0700

    Initial Commit

---
+++ FizzBuzz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FizzBuzz
+//
+//  Created by Julien Guanzon on 1/28/15.
+//  Copyright (c) 2015 Julien Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FizzBuzz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FizzBuzz
+//
+//  Created by Julien Guanzon on 1/28/15.
+//  Copyright (c) 2015 Julien Guanzon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
